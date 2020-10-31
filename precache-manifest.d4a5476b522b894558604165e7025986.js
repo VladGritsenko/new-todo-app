ded9b5a0100ad164203c0103091eb0070edc85b2
VladGritsenko <vladik.grytsenko2017@gmail.com>
2020-10-31 19:28:29 +0200

    Updates

---
+++ precache-manifest.d4a5476b522b894558604165e7025986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b489b54807d8469f5eb3a164188b425",
+    "url": "/new-todo-app/index.html"
+  },
+  {
+    "revision": "dc590b4da17032450dab",
+    "url": "/new-todo-app/static/css/main.bbd3f968.chunk.css"
+  },
+  {
+    "revision": "a155803b7b433e76054c",
+    "url": "/new-todo-app/static/js/2.71a5309d.chunk.js"
+  },
+  {
+    "revision": "22944df67a1e9cf588daa5255143530c",
+    "url": "/new-todo-app/static/js/2.71a5309d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc590b4da17032450dab",
+    "url": "/new-todo-app/static/js/main.44e7a4eb.chunk.js"
+  },
+  {
+    "revision": "2f92c7254cee20b5c76c",
+    "url": "/new-todo-app/static/js/runtime-main.bea7bc6d.js"
+  }
+]);
